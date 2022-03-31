4189bb63ec723f5f48b90ed3f6119cec2f826ccb
martin835 <64438132+martin835@users.noreply.github.com>
2022-03-31 10:02:34 +0200

    Update App.js
    small change in NavBar component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,50 @@
-import "./App.css"
-import MyNavbar from "./components/MyNavbar"
-import MainSection from "./components/MainSection"
-import { useEffect, useState } from "react"
-import { HashRouter, Routes, Route } from "react-router-dom"
-import NewsMain from "./components/NewsMain"
+import "./App.css";
+import MyNavbar from "./components/MyNavbar";
+import MainSection from "./components/MainSection";
+import { useEffect, useState } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import NewsMain from "./components/NewsMain";
 
 function App() {
-  const [img, setImg] = useState()
-  const [fet, setFet] = useState()
+  const [img, setImg] = useState();
+  const [fet, setFet] = useState();
   // useEffect(changeImg)
 
   useEffect(() => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    imageRendered = !imageRendered
+    imageRendered = !imageRendered;
+  });
 
-  })
+  let imageRendered = true;
 
-  let imageRendered = true
-
-  const changeImg = (value) => {
+  /* const changeImg = (value) => {
     setImg(value)
     // console.log(value)
-  }
+  } */
 
   const changeFet = (value) => {
-    setFet(value)
-  }
-
-
+    setFet(value);
+  };
 
   return (
     <HashRouter basename="/">
-      {imageRendered ? <MyNavbar image={img} funcD={fet} /> : <MyNavbar funcD={fet} />}
+      {imageRendered ? (
+        <MyNavbar image={img} funcD={fet} />
+      ) : (
+        <MyNavbar funcD={fet} />
+      )}
       <Routes>
-        <Route path="/" element={<NewsMain changeImg={changeImg} />} />
-        <Route path="/profile/:profileId" element={<MainSection D={changeFet} />} />
+        <Route
+          path="/"
+          element={<NewsMain changeImg={(value) => setImg(value)} />}
+        />
+        <Route
+          path="/profile/:profileId"
+          element={<MainSection D={changeFet} />}
+        />
       </Routes>
     </HashRouter>
-  )
+  );
 }
 
-export default App
+export default App;
